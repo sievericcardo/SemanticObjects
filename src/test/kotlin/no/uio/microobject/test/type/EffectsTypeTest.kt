74d7ac2a850197a88c66f7f046d638b116cc8980
Riccardo Sieve <sievericcardo@gmail.com>
2026-04-09 12:01:24 +0200

    Extended tests

--- src/test/kotlin/no/uio/microobject/test/type/EffectsTypeTest.kt
+++ src/test/kotlin/no/uio/microobject/test/type/EffectsTypeTest.kt
@@ -11,7 +11,7 @@ class EffectsTypeTest : MicroObjectTypeTest() {
             assertTrue(tC.report(false))
         }
         
-        "Effects override with same effects should pass" {
+        "Effects override with same or fewer effects should pass" {
             val tC = checkClass("SmartDevice", "effectsTest")
             // This should pass - override with same or fewer effects
             assertTrue(tC.report(false))
@@ -20,6 +20,12 @@ class EffectsTypeTest : MicroObjectTypeTest() {
         "Effects override with additional effects should fail" {
             val tC = checkClass("AdvancedDevice", "effectsTest")
             // This should fail - override adds new effects
+            assertFalse(tC.report(false))
+        }
+
+        "Base method must have effects if override has effects" {
+            val tC = checkClass("AdvanceBaseDevice", "effectsTest")
+            // This should pass - override with fewer effects
             assertFalse(tC.report(false))
         }
     }
